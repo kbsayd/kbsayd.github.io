90ee0fee82ef0b62e375ae07443a1a20ae08609e
lifeiyu <ccxjiao@163.com>
2022-06-22 11:49:39 +0800

    lfy 第二

--- src/main/java/com/gym/gymxmjpa/controller/chongzhiController.java
+++ src/main/java/com/gym/gymxmjpa/controller/chongzhiController.java
@@ -68,7 +68,7 @@ public class chongzhiController {
         return "WEB-INF/jsp/ShouRuTongji";
     }
 
-    @RequestMapping("/jin3")
+    @RequestMapping("/jin4")
     public String jin3(){
         return "WEB-INF/jsp/ShouRuTongji";
     }
